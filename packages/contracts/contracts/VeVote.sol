4c4520b56d924f429917a509aefdd695570b0fa8
Roisin Dowling <55144940+roisindowling@users.noreply.github.com>
2025-02-28 20:02:55 +1100

    Add VeVote Proxy contract and UUPS deploy scripts (#20)
    * Add VeVote Proxy Contract
    
    * Add VeVote Proxy contract
    
    * Make draft version of VeVote upgradeable
    
    * Add scripts for dpeloying UUPS contracts

--- packages/contracts/contracts/VeVote.sol
+++ packages/contracts/contracts/VeVote.sol
@@ -1,18 +1,31 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.20;
+pragma solidity 0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract VeVote is ERC20, Ownable {
-    constructor(
-        address initialOwner
-    ) ERC20("VeVote", "VV") Ownable(initialOwner) {
+contract VeVote is ERC20Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
+    function initialize() public initializer { 
+        __ERC20_init("VeVote", "VV");
+        __Ownable_init(msg.sender);
         _mint(msg.sender, 100000 * 10 ** decimals());
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
+
+    /// @dev Authorized upgrading of the contract implementation
+    /// @dev Only callable by the upgrader role
+    /// @param newImplementation Address of the new contract implementation
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+    /// @notice Returns the version of the contract
+    /// @dev This should be updated every time a new version of implementation is deployed
+  /// @return string The version of the contract
+function version() public pure virtual returns (string memory) {
+    return "1";
+  }
 }
---
+++ packages/contracts/scripts/types/deployAndUpgrade.ts
@@ -0,0 +1,6 @@
+export type DeployUpgradeOptions = {
+    libraries?: ({ [libraryName: string]: string } | undefined)[]
+    versions?: (number | undefined)[]
+    logOutput?: boolean
+  }
+  
