0ebddc961c62a9ecebb8ccbfcf359445fe48b102
Hamza Abdullahi <hamza.abdullahi@vechain.org>
2025-03-19 13:23:15 +0000

    fix: update workspace check and input default to 'prd'

--- terraform/vercel-frontend.tf
+++ terraform/vercel-frontend.tf
@@ -1,5 +1,5 @@
 resource "vercel_project" "vevote_frontend" {
-  count     = terraform.workspace == "prod" ? 1 : 0
+  count     = terraform.workspace == "prd" ? 1 : 0
   name      = "vevote-frontend"
   team_id   = local.env.vercel_team_id
   framework = "nextjs"
@@ -12,7 +12,7 @@ resource "vercel_project" "vevote_frontend" {
 }
 
 resource "vercel_deployment" "vevote_frontend_deployment" {
-  count      = terraform.workspace == "prod" ? 1 : 0
+  count      = terraform.workspace == "prd" ? 1 : 0
   project_id = vercel_project.vevote_frontend[0].id
   team_id    = local.env.vercel_team_id
   production = true
