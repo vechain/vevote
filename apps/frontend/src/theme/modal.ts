8febef6f23f65bb056e12aefc45c03b2587ca32a
LLPROG <lucalobianco@hotmail.it>
2025-03-26 08:30:52 +0100

    add modal

---
+++ apps/frontend/src/theme/modal.ts
@@ -0,0 +1,48 @@
+import { createMultiStyleConfigHelpers } from "@chakra-ui/react";
+import { modalAnatomy } from "@chakra-ui/anatomy";
+
+const { definePartsStyle, defineMultiStyleConfig } = createMultiStyleConfigHelpers(modalAnatomy.keys);
+
+const resetModalItemsStyle = {
+  color: { color: "primary.900" },
+  padding: { padding: "0" },
+};
+
+const defaultVariant = definePartsStyle({
+  dialog: {
+    bg: "white",
+    borderRadius: "16px",
+    display: "flex",
+    flexDirection: "column",
+    gap: "40px",
+    boxShadow: "0px 2px 6px 0px rgba(0, 0, 0, 0.07)",
+  },
+  overlay: {
+    backdropFilter: "blur(15px)",
+    backgroundColor: "rgba(23, 13, 69, 0.25)",
+  },
+  header: {
+    fontSize: "30px",
+    fontWeight: "600",
+    color: "primary.500",
+  },
+  closeButton: resetModalItemsStyle.color,
+  body: resetModalItemsStyle.color,
+  footer: resetModalItemsStyle.color,
+});
+
+export const ModalStyle = defineMultiStyleConfig({
+  variants: { default: defaultVariant },
+  sizes: {
+    default: {
+      dialog: {
+        padding: "40px",
+        maxWidth: "480px",
+      },
+      header: resetModalItemsStyle.padding,
+      body: resetModalItemsStyle.padding,
+      footer: resetModalItemsStyle.padding,
+    },
+  },
+  defaultProps: { variant: "default", size: "default" },
+});
