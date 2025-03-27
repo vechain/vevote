040725e7a0c48018f21694e7b49cbd08a0ca98a1
Hamza Abdullahi <hamza.abdullahi@vechain.org>
2025-03-27 15:40:37 +0000

    Amending framework and root directory

--- terraform/vercel-frontend.tf
+++ terraform/vercel-frontend.tf
@@ -2,13 +2,16 @@ resource "vercel_project" "vevote_frontend" {
   count     = terraform.workspace == "prod" ? 1 : 0
   name      = "${local.config.project}-frontend-${local.environment}"
   team_id   = local.config.vercel_team_id
-  framework = "nextjs"
+  framework = "vite"
   git_repository = {
     type              = "github"
     repo              = "vechain/vevote"
     production_branch = "main"
   }
-  root_directory = "apps/frontend"
+  root_directory   = "apps/"
+  build_command    = "yarn build:staging"
+  dev_command      = "yarn dev:staging"
+  output_directory = "frontend/dist"
 }
 
 resource "vercel_deployment" "vevote_frontend_deployment" {
