64b46a8c1f6db2d874da3dbbfa267d41d893bc5b
LLPROG <lucalobianco@hotmail.it>
2025-04-02 15:00:51 +0200

    Proposals page as home

--- apps/frontend/src/pages/Common/Navigation.tsx
+++ apps/frontend/src/pages/Common/Navigation.tsx
@@ -1,6 +1,5 @@
 import { Header } from "@/components/navbar/Header";
 import { Route, Routes, useLocation } from "react-router-dom";
-import { Home } from "../Home";
 import { Proposals } from "../Proposals";
 import { CreateProposal } from "../CreateProposal/CreateProposal";
 import { Footer } from "@/components/footer/Footer";
@@ -14,8 +13,7 @@ export const Navigation = () => {
     <>
       {!excludedRoutes.includes(pathname) && <Header />}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/proposals" element={<Proposals />} />
+        <Route path="/" element={<Proposals />} />
         <Route path="/create-proposal" element={<CreateProposal />} />
       </Routes>
       {!excludedRoutes.includes(pathname) && <Footer />}
