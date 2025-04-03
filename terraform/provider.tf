b40ad66330227f7430fc22f7ec2b348fbb5d4441
Hamza Abdullahi <hamza.abdullahi@vechain.org>
2025-04-03 18:22:15 +0100

    vercel provider to use 2.15 and add env var

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -17,7 +17,7 @@ terraform {
   required_providers {
     vercel = {
       source  = "vercel/vercel"
-      version = "~> 0.3"
+      version = "~> 2.15"
     }
   }
 }
--- terraform/vercel-frontend.tf
+++ terraform/vercel-frontend.tf
@@ -1,6 +1,5 @@
 resource "vercel_project" "vevote_frontend" {
-  count     = terraform.workspace == "prod" ? 1 : 0
-  name      = "${local.config.project}-frontend"
+  name      = "${local.config.project}-frontend-dev-experimental"
   team_id   = local.config.vercel_team_id
   framework = "vite"
   git_repository = {
@@ -8,39 +7,49 @@ resource "vercel_project" "vevote_frontend" {
     repo              = "vechain/vevote"
     production_branch = "main"
   }
-  root_directory   = "apps/"
-  build_command    = "yarn build:staging"
+  root_directory   = "apps"
+  build_command    = "yarn clean && yarn build:staging"
   dev_command      = "yarn dev:staging"
   output_directory = "frontend/dist"
 
-  environment = [
-    {
-      target = ["production", "preview"]
-      key    = "VECHAIN_URL_DEVNET"
-      value  = local.config.all_environments.vechain_url_devnet
-    },
-    {
-      target = ["production", "preview"]
-      key    = "PUBLIC_IPFS_PINNING_SERVICE"
-      value  = local.config.all_environments.public_ipfs_pinning_service
-    },
-    {
-      target = ["production", "preview"]
-      key    = "DEVNET_STAGING_MNEMONIC"
-      value  = local.config.all_environments.devnet_staging_mnemonic
-    },
-    {
-      target = ["production", "preview"]
-      key    = "MNEMONIC"
-      value  = local.config.all_environments.mnemonic
-    }
-  ]
+  lifecycle {
+    ignore_changes = [
+      enable_affected_projects_deployments
+    ]
+  }
+}
+
+resource "vercel_project_environment_variable" "vechain_url_devnet" {
+  project_id = vercel_project.vevote_frontend.id
+  key        = "VECHAIN_URL_DEVNET"
+  value      = local.config.all_environments.vechain_url_devnet
+  target     = ["production", "preview"]
+}
+
+resource "vercel_project_environment_variable" "public_ipfs_pinning_service" {
+  project_id = vercel_project.vevote_frontend.id
+  key        = "PUBLIC_IPFS_PINNING_SERVICE"
+  value      = local.config.all_environments.public_ipfs_pinning_service
+  target     = ["production", "preview"]
+}
+
+resource "vercel_project_environment_variable" "devnet_staging_mnemonic" {
+  project_id = vercel_project.vevote_frontend.id
+  key        = "DEVNET_STAGING_MNEMONIC"
+  value      = local.config.all_environments.devnet_staging_mnemonic
+  target     = ["production", "preview"]
+}
+
+resource "vercel_project_environment_variable" "mnemonic" {
+  project_id = vercel_project.vevote_frontend.id
+  key        = "MNEMONIC"
+  value      = local.config.all_environments.mnemonic
+  target     = ["production", "preview"]
 }
 
 resource "vercel_deployment" "vevote_frontend_deployment" {
-  count      = terraform.workspace == "prod" ? 1 : 0
-  project_id = vercel_project.vevote_frontend[0].id
+  project_id = vercel_project.vevote_frontend.id
   team_id    = local.config.vercel_team_id
-  production = true
-  ref        = local.config.tag
+  production = false
+  ref        = "main"
 }
