b762fbf2919734b068d0e199ee5acc0c911d5a83
roisindowling <roisin.dowling@vechain.org>
2025-05-20 16:34:27 +0100

    add file to manage mnemenic

--- packages/config/contracts/index.ts
+++ packages/config/contracts/index.ts
@@ -1,42 +1,41 @@
-export * from "./type";
+export * from "./type"
 
-import { createLocalConfig } from "./envs/local";
-import { createE2EConfig } from "./envs/e2e";
-import { createTestnetConfig } from "./envs/testnet";
-import { createMainnetConfig } from "./envs/mainnet";
-import { createTestnetStagingConfig } from "./envs/testnetStaging";
-import { createGalacticaTestConfig } from "./envs/galaticaTest";
+import { createLocalConfig } from "./envs/local"
+import { createTestnetStagingConfig } from "./envs/testnetStaging"
+import { createE2EConfig } from "./envs/e2e"
+import { createTestnetConfig } from "./envs/testnet"
+import { createMainnetConfig } from "./envs/mainnet"
+import { createGalacticaTestConfig } from "./envs/galaticaTest"
 
-export const EnvConfigValues = ["local", "e2e", "testnet", "mainnet", "testnet-staging", "galactica-test"] as const;
-export type EnvConfig = (typeof EnvConfigValues)[number];
+export const AppEnv = {
+  LOCAL: "local",
+  E2E: "e2e",
+  TESTNET_STAGING: "testnet-staging",
+  TESTNET: "testnet",
+  MAINNET: "mainnet",
+  GALACTICA_TEST: "galactica-test",
+} as const
+
+export const EnvConfigValues = Object.values(AppEnv)
+export type EnvConfig = (typeof EnvConfigValues)[number]
 
 export function getContractsConfig(env: EnvConfig) {
   switch (env) {
-    case "local":
-      return createLocalConfig();
-    case "e2e":
-      return createE2EConfig();
-    case "testnet":
-      return createTestnetConfig();
-    case "mainnet":
-      return createMainnetConfig();
-    case "testnet-staging":
-      return createTestnetStagingConfig();
-    case "galactica-test":
-      return createGalacticaTestConfig();
-  
+    case AppEnv.LOCAL:
+      return createLocalConfig()
+    case AppEnv.E2E:
+      return createE2EConfig()
+    case AppEnv.TESTNET_STAGING:
+      return createTestnetStagingConfig()
+    case AppEnv.TESTNET:
+      return createTestnetConfig()
+    case AppEnv.MAINNET:
+      return createMainnetConfig()
+    case AppEnv.GALACTICA_TEST:
+      return createGalacticaTestConfig()
 
     default:
-      throw new Error(`Invalid ENV "${env}"`);
+      throw new Error(`Invalid ENV "${env}"`)
   }
 }
 
-export function shouldRunSimulation() {
-  return (
-    process.env.VITE_APP_ENV == "local" && process.env.RUN_SIMULATION === "true"
-  );
-}
-
-export function isE2E() {
-  return process.env.VITE_APP_ENV == "e2e";
-}
