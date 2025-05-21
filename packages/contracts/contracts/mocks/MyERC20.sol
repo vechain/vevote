dc7543ada3e4c32f01bf0aaae0bd170e18716cb8
roisindowling <roisin.dowling@vechain.org>
2025-05-21 16:28:22 +0100

    add ERC20 mock for VTHO

---
+++ packages/contracts/contracts/mocks/MyERC20.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyERC20 is ERC20, ERC20Burnable, Ownable {
+    constructor(
+        address recipient,
+        address initialOwner
+    ) ERC20("MyERC20", "MTK") Ownable(initialOwner) {
+        _mint(recipient, 10000000000000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
