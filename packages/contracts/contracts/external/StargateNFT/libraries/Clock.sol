224cb00919ebc6ab18b88e54acb780607776a764
roisindowling <roisin.dowling@vechain.org>
2025-05-28 13:51:54 +0900

    format fixes stargate

--- packages/contracts/contracts/external/StargateNFT/libraries/Clock.sol
+++ packages/contracts/contracts/external/StargateNFT/libraries/Clock.sol
@@ -11,50 +11,50 @@
 
 pragma solidity 0.8.20;
 
-import {Time} from "@openzeppelin/contracts/utils/types/Time.sol";
+import { Time } from "@openzeppelin/contracts/utils/types/Time.sol";
 
 /// @title Clock
 /// @notice Library for the StargateNFT contract to get the current block number and clock mode
 /// @dev The library exposes external functions but also internal functions that are used by other libraries
 /// to avoid external calls and avoid the need to make this library a dependency of other libraries.
 library Clock {
-    // ------------------ Getters ------------------ //
+  // ------------------ Getters ------------------ //
 
-    /// @notice Returns the current block number
-    /// @return blockNumber - the current block number
-    function clock() external view returns (uint48) {
-        return _clock();
-    }
+  /// @notice Returns the current block number
+  /// @return blockNumber - the current block number
+  function clock() external view returns (uint48) {
+    return _clock();
+  }
 
-    /// @notice Returns the current timestamp
-    /// @return timestamp - the current timestamp
-    function timestamp() external view returns (uint48) {
-        return _timestamp();
-    }
+  /// @notice Returns the current timestamp
+  /// @return timestamp - the current timestamp
+  function timestamp() external view returns (uint48) {
+    return _timestamp();
+  }
 
-    /// @notice Returns the mode of the clock
-    /// @return mode - the mode of the clock
-    function CLOCK_MODE() external pure returns (string memory) {
-        return _CLOCK_MODE();
-    }
+  /// @notice Returns the mode of the clock
+  /// @return mode - the mode of the clock
+  function CLOCK_MODE() external pure returns (string memory) {
+    return _CLOCK_MODE();
+  }
 
-    // ------------------ Internal ------------------ //
+  // ------------------ Internal ------------------ //
 
-    /// @notice Returns the current block number
-    /// @return blockNumber - the current block number
-    function _clock() internal view returns (uint48) {
-        return Time.blockNumber();
-    }
+  /// @notice Returns the current block number
+  /// @return blockNumber - the current block number
+  function _clock() internal view returns (uint48) {
+    return Time.blockNumber();
+  }
 
-    /// @notice Returns the mode of the clock
-    /// @return mode - the mode of the clock
-    function _CLOCK_MODE() internal pure returns (string memory) {
-        return "mode=blocknumber&from=default";
-    }
+  /// @notice Returns the mode of the clock
+  /// @return mode - the mode of the clock
+  function _CLOCK_MODE() internal pure returns (string memory) {
+    return "mode=blocknumber&from=default";
+  }
 
-    /// @notice Returns the current timestamp
-    /// @return timestamp - the current timestamp
-    function _timestamp() internal view returns (uint48) {
-        return Time.timestamp();
-    }
+  /// @notice Returns the current timestamp
+  /// @return timestamp - the current timestamp
+  function _timestamp() internal view returns (uint48) {
+    return Time.timestamp();
+  }
 }
--- packages/contracts/contracts/external/StargateNFT/libraries/Settings.sol
+++ packages/contracts/contracts/external/StargateNFT/libraries/Settings.sol
@@ -11,112 +11,98 @@
 
 pragma solidity 0.8.20;
 
-import {DataTypes} from "./DataTypes.sol";
-import {IStargateNFT} from "../../../interfaces/IStargateNFT.sol";
-import {IStargateDelegation} from "../../../interfaces/IStargateDelegation.sol";
-import {ITokenAuction} from "../../../interfaces/ITokenAuction.sol";
-import {Errors} from "./Errors.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {Levels} from "./Levels.sol";
+import { DataTypes } from "./DataTypes.sol";
+import { IStargateNFT } from "../../../interfaces/IStargateNFT.sol";
+import { IStargateDelegation } from "../../../interfaces/IStargateDelegation.sol";
+import { ITokenAuction } from "../../../interfaces/ITokenAuction.sol";
+import { Errors } from "./Errors.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { Levels } from "./Levels.sol";
 
 /// @title Settings
 /// @notice Library for the StargateNFT contract to handle settings and configuration
 library Settings {
-    // ------------------ Events ------------------ //
-    /**
-     * @notice Emitted when the admin changes the address of a contract
-     * @param oldContractAddress The old contract address
-     * @param newContractAddress The new contract address
-     */
-    event ContractAddressUpdated(
-        address oldContractAddress,
-        address newContractAddress,
-        string contractName
-    );
+  // ------------------ Events ------------------ //
+  /**
+   * @notice Emitted when the admin changes the address of a contract
+   * @param oldContractAddress The old contract address
+   * @param newContractAddress The new contract address
+   */
+  event ContractAddressUpdated(address oldContractAddress, address newContractAddress, string contractName);
 
-    /**
-     * @notice Emitted when the base URI for the token metadata is updated
-     * @param oldBaseURI The old base URI
-     * @param newBaseURI The new base URI
-     */
-    event BaseURIUpdated(string oldBaseURI, string newBaseURI);
+  /**
+   * @notice Emitted when the base URI for the token metadata is updated
+   * @param oldBaseURI The old base URI
+   * @param newBaseURI The new base URI
+   */
+  event BaseURIUpdated(string oldBaseURI, string newBaseURI);
 
-    /**
-     * @notice Emitted when the VTHO generation end timestamp is updated
-     * @param vthoGenerationEndTimestamp The new VTHO generation end timestamp
-     */
-    event VthoGenerationEndTimestampSet(uint48 vthoGenerationEndTimestamp);
+  /**
+   * @notice Emitted when the VTHO generation end timestamp is updated
+   * @param vthoGenerationEndTimestamp The new VTHO generation end timestamp
+   */
+  event VthoGenerationEndTimestampSet(uint48 vthoGenerationEndTimestamp);
 
-    // ------------------ Setters ------------------ //
+  // ------------------ Setters ------------------ //
 
-    /// @notice Sets the legacy nodes contract address, ie TokenAuction
-    /// @dev Only the DEFAULT_ADMIN_ROLE can call this function
-    /// @param _legacyNodes - the new legacy nodes address
-    function setLegacyNodes(DataTypes.StargateNFTStorage storage $, address _legacyNodes) external {
-        if (_legacyNodes == address(0)) {
-            revert Errors.AddressCannotBeZero();
-        }
-
-        emit ContractAddressUpdated(address($.legacyNodes), _legacyNodes, "legacyNodes");
-        $.legacyNodes = ITokenAuction(_legacyNodes);
+  /// @notice Sets the legacy nodes contract address, ie TokenAuction
+  /// @dev Only the DEFAULT_ADMIN_ROLE can call this function
+  /// @param _legacyNodes - the new legacy nodes address
+  function setLegacyNodes(DataTypes.StargateNFTStorage storage $, address _legacyNodes) external {
+    if (_legacyNodes == address(0)) {
+      revert Errors.AddressCannotBeZero();
     }
 
-    /// @notice Sets the stargate delegation contract address
-    /// @dev Only the DEFAULT_ADMIN_ROLE can call this function
-    /// @param _stargateDelegation - the new stargate delegation address
-    function setStargateDelegation(
-        DataTypes.StargateNFTStorage storage $,
-        address _stargateDelegation
-    ) external {
-        if (_stargateDelegation == address(0)) {
-            revert Errors.AddressCannotBeZero();
-        }
+    emit ContractAddressUpdated(address($.legacyNodes), _legacyNodes, "legacyNodes");
+    $.legacyNodes = ITokenAuction(_legacyNodes);
+  }
 
-        emit ContractAddressUpdated(
-            address($.stargateDelegation),
-            _stargateDelegation,
-            "stargateDelegation"
-        );
-        $.stargateDelegation = IStargateDelegation(_stargateDelegation);
+  /// @notice Sets the stargate delegation contract address
+  /// @dev Only the DEFAULT_ADMIN_ROLE can call this function
+  /// @param _stargateDelegation - the new stargate delegation address
+  function setStargateDelegation(DataTypes.StargateNFTStorage storage $, address _stargateDelegation) external {
+    if (_stargateDelegation == address(0)) {
+      revert Errors.AddressCannotBeZero();
     }
 
-    /// @notice Set the VTHO token address
-    /// @param $ Storage pointer
-    /// @param _vthoToken The address of the VTHO token contract
-    function setVthoToken(DataTypes.StargateNFTStorage storage $, address _vthoToken) public {
-        if (_vthoToken == address(0)) {
-            revert Errors.AddressCannotBeZero();
-        }
-        emit ContractAddressUpdated(address($.vthoToken), _vthoToken, "vthoToken");
-        $.vthoToken = IERC20(_vthoToken);
-    }
+    emit ContractAddressUpdated(address($.stargateDelegation), _stargateDelegation, "stargateDelegation");
+    $.stargateDelegation = IStargateDelegation(_stargateDelegation);
+  }
 
-    /// @notice Sets the base URI for the token collection metadata
-    /// @dev Only the DEFAULT_ADMIN_ROLE can call this function
-    /// @dev Metadata is hosted on IPFS, and the base URI is the base path to the collection metadata
-    /// @param _baseTokenURI The new base URI
-    /// @dev Emits a {IStargateNFT.BaseURIUpdated} event
-    function setBaseURI(
-        DataTypes.StargateNFTStorage storage $,
-        string memory _baseTokenURI
-    ) external {
-        emit BaseURIUpdated($.baseTokenURI, _baseTokenURI);
-        $.baseTokenURI = _baseTokenURI;
+  /// @notice Set the VTHO token address
+  /// @param $ Storage pointer
+  /// @param _vthoToken The address of the VTHO token contract
+  function setVthoToken(DataTypes.StargateNFTStorage storage $, address _vthoToken) public {
+    if (_vthoToken == address(0)) {
+      revert Errors.AddressCannotBeZero();
     }
+    emit ContractAddressUpdated(address($.vthoToken), _vthoToken, "vthoToken");
+    $.vthoToken = IERC20(_vthoToken);
+  }
 
-    /// @notice Set the timestamp when the protocol will stop generating VTHO by holding VET
-    /// @dev By default it will be 0, meaning the protocol will keep generating VTHO indefinitely,
-    /// once the VeChain foundation will know the exact timestamp when the VTHO generation will stop
-    /// we will set this value. It can be set back to 0, but then rewards will be recalculated based on the
-    /// current timestamp. So set it back to 0 with caution.
-    /// @param _vthoGenerationEndTimestamp The timestamp when the protocol will stop generating VTHO by holding VET
-    /// @dev Emits a {IStargateNFT.VthoGenerationEndTimestampUpdated} event
-    function setVthoGenerationEndTimestamp(
-        DataTypes.StargateNFTStorage storage $,
-        uint48 _vthoGenerationEndTimestamp
-    ) external {
-        $.vthoGenerationEndTimestamp = _vthoGenerationEndTimestamp;
+  /// @notice Sets the base URI for the token collection metadata
+  /// @dev Only the DEFAULT_ADMIN_ROLE can call this function
+  /// @dev Metadata is hosted on IPFS, and the base URI is the base path to the collection metadata
+  /// @param _baseTokenURI The new base URI
+  /// @dev Emits a {IStargateNFT.BaseURIUpdated} event
+  function setBaseURI(DataTypes.StargateNFTStorage storage $, string memory _baseTokenURI) external {
+    emit BaseURIUpdated($.baseTokenURI, _baseTokenURI);
+    $.baseTokenURI = _baseTokenURI;
+  }
 
-        emit VthoGenerationEndTimestampSet(_vthoGenerationEndTimestamp);
-    }
+  /// @notice Set the timestamp when the protocol will stop generating VTHO by holding VET
+  /// @dev By default it will be 0, meaning the protocol will keep generating VTHO indefinitely,
+  /// once the VeChain foundation will know the exact timestamp when the VTHO generation will stop
+  /// we will set this value. It can be set back to 0, but then rewards will be recalculated based on the
+  /// current timestamp. So set it back to 0 with caution.
+  /// @param _vthoGenerationEndTimestamp The timestamp when the protocol will stop generating VTHO by holding VET
+  /// @dev Emits a {IStargateNFT.VthoGenerationEndTimestampUpdated} event
+  function setVthoGenerationEndTimestamp(
+    DataTypes.StargateNFTStorage storage $,
+    uint48 _vthoGenerationEndTimestamp
+  ) external {
+    $.vthoGenerationEndTimestamp = _vthoGenerationEndTimestamp;
+
+    emit VthoGenerationEndTimestampSet(_vthoGenerationEndTimestamp);
+  }
 }
