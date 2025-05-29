c1958babdf9a841b6c032e215b7588a346b51be7
roisindowling <roisin.dowling@vechain.org>
2025-05-29 14:52:56 +0900

    update storage types

--- packages/contracts/contracts/governance/libraries/VeVoteStorageTypes.sol
+++ packages/contracts/contracts/governance/libraries/VeVoteStorageTypes.sol
@@ -4,8 +4,7 @@ pragma solidity 0.8.20;
 
 import { VeVoteTypes } from "./VeVoteTypes.sol";
 import { INodeManagement } from "../../interfaces/INodeManagement.sol";
-import { ITokenAuction } from "../../interfaces/ITokenAuction.sol";
-import { VechainNodesDataTypes } from "../../libraries/VechainNodesDataTypes.sol";
+import { IStargateNFT } from "../../interfaces/IStargateNFT.sol";
 import "@openzeppelin/contracts/utils/structs/DoubleEndedQueue.sol";
 import "@openzeppelin/contracts/utils/structs/Checkpoints.sol";
 
@@ -16,32 +15,36 @@ library VeVoteStorageTypes {
     // ------------------------------- Version 1 -------------------------------
     // ------------------------------- General Storage -------------------------------
     mapping(uint256 proposalId => VeVoteTypes.ProposalCore) proposals;
-    /// @notice Minimum delay before a proposal can be voted on after creation
+    /// @notice Minimum delay before a proposal can be voted on after creation (blocks)
     uint48 minVotingDelay;
-    /// @notice Minimum duration a vote must remain open
+    /// @notice Minimum duration a vote must remain open (blocks)
     uint48 minVotingDuration;
-    /// @notice Maximum duration a vote can remain open
+    /// @notice Maximum duration a vote can remain open (blocks)
     uint48 maxVotingDuration;
     /// @notice Maximum number of choices allowed in a proposal
     uint8 maxChoices;
     // ------------------------------- Quorum Storage -------------------------------
-    /// @notice Stores history of quorum numerator changes
+    /// @notice Stores history of quorum numerator changes (blocks)
     Checkpoints.Trace208 quorumNumeratorHistory;
     // ------------------------------- External Contracts Storage -------------------------------
     /// @notice Node Management contract for fetching voter eligibility and node information
     INodeManagement nodeManagement;
-    /// @notice Vechain Node Token Auction contract (Optional - Pending Confirmation)
-    ITokenAuction vechainNodesContract; // TODO: Do we need this, or can we just use the nodeManagement contract?
+    /// @notice Stargate NFT contract
+    IStargateNFT stargateNFT;
+    /// @notice Builtin Authority contract address. After the Hayabusa mainnet hardfork, this will point to the on-chain staking registry.
+    address validatorContract;
     // ------------------------------- Voting Storage -------------------------------
-    /// @notice Voting weight multiplier for different node levels
-    mapping(VechainNodesDataTypes.NodeStrengthLevel => uint256) nodeMultiplier;
+    /// @notice Voting weight multiplier for different stargate NFTs levelIds
+    mapping(uint8 => uint256) levelIdMultiplier;
     /// @notice Vote tally for each proposal choice
     mapping(uint256 => mapping(uint8 => uint256)) voteTally;
     /// @notice Stores the votes for each proposal (bitmask representation)
     mapping(uint256 => mapping(address => uint32)) votes;
     /// @notice Stores the total votes for each proposal
     mapping(uint256 => uint256) totalVotes;
-    /// @notice Stores the cheapest Node a user can obtain
-    uint8 baseLevelNode; // TODO: DO WE NEED THIS?
+    /// @notice Tracks historical minimum staked VET required for normalization
+    Checkpoints.Trace208 minStakedVetHistory;
+    /// @notice Stores nodes that have voted on a given proposal
+    mapping(uint256 => mapping(uint256 => bool)) nodeHasVoted;
   }
 }
