1c48e7c76652bd3a2ab1c841120aa76bfe725d95
LLPROG <lucalobianco@hotmail.it>
2025-06-16 13:55:52 +0200

    Copy confirmation

--- apps/frontend/src/components/ui/CopyLink.tsx
+++ apps/frontend/src/components/ui/CopyLink.tsx
@@ -1,6 +1,8 @@
 import { CopyIcon } from "@/icons";
-import { Button, Flex, Icon, Link, LinkProps } from "@chakra-ui/react";
+import { Button, Flex, Icon, Link, LinkProps, useToast } from "@chakra-ui/react";
 import { useCallback } from "react";
+import { InfoBox } from "./InfoBox";
+import { useI18nContext } from "@/i18n/i18n-react";
 
 export const CopyLink = ({
   textToCopy,
@@ -8,10 +10,20 @@ export const CopyLink = ({
 }: LinkProps & {
   textToCopy?: string;
 }) => {
-  const copy = useCallback(() => {
+  const { LL } = useI18nContext();
+  const toast = useToast();
+  const copy = useCallback(async () => {
     if (!textToCopy) return;
-    navigator.clipboard.writeText(textToCopy || "");
-  }, [textToCopy]);
+    await navigator.clipboard.writeText(textToCopy || "");
+    toast({
+      position: "bottom",
+      render: () => (
+        <InfoBox padding={3} alignItems={"center"} variant="approved">
+          {LL.copied_to_clipboard()}
+        </InfoBox>
+      ),
+    });
+  }, [LL, textToCopy, toast]);
   return (
     <Flex gap={2} alignItems={"center"}>
       <Link {...props} />
