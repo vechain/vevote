80706c1827a99d53c7860d35a844650d8f1e8016
roisindowling <roisin.dowling@vechain.org>
2025-07-02 18:47:25 +0100

    remove unused multipliers

--- packages/contracts/contracts/governance/libraries/VeVoteConstants.sol
+++ packages/contracts/contracts/governance/libraries/VeVoteConstants.sol
@@ -23,16 +23,4 @@ library VeVoteConstants {
 
   /// @dev The minimum staked VET required to become a validator (25 million VET in wei).
   uint256 internal constant VALIDATOR_STAKED_VET_REQUIREMENT = 25_000_000 ether;
-
-  // ---------------- State Bitmap Constants ----------------
-
-  /// @notice Bitmap representing all possible proposal states.
-  /// @dev Used for filtering proposal states via bitwise operations.
-  bytes32 internal constant ALL_PROPOSAL_STATES_BITMAP =
-    bytes32((1 << (uint8(type(VeVoteTypes.ProposalState).max) + 1)) - 1);
-
-  // ---------------- Voting Constants ----------------
-
-  /// @dev Multiplier scale used to normalize weighted voting.
-  uint256 internal constant VOTING_MULTIPLIER_SCALE = 100;
 }
