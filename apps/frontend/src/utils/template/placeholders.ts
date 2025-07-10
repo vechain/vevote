2c59be38f6777ff0f0479af22855d9937fe4b7a8
LLPROG <lucalobianco@hotmail.it>
2025-07-10 15:19:41 +0200

    improve template

---
+++ apps/frontend/src/utils/template/placeholders.ts
@@ -0,0 +1,39 @@
+import dayjs from "dayjs";
+
+export const PROPOSAL_TYPES_PLACEHOLDER = {
+  onChain: "[On-chain Action]",
+  textOnly: "[Text-only Proposal]",
+};
+
+export const PROPOSAL_SUMMARY_PLACEHOLDER =
+  "[Provide a brief overview of the proposal in 2-3 sentences. This should capture the essence of what you're proposing and why it matters to VebetterDAO.]";
+
+export const MOTIVATION_PLACEHOLDER =
+  "[Explain the problem or opportunity that this proposal addresses. Why is this proposal important for VebetterDAO? What are the potential benefits?]";
+
+export const DETAILS_SPECIFICATION_PLACEHOLDER =
+  "[Provide a detailed description of the proposal. Include technical details, methodologies, and any other relevant information. Break this section into sub-sections if necessary.]";
+
+export const GOALS_PLACEHOLDER = "[List the specific goals and outcomes you aim to achieve with this proposal.]";
+
+export const RISK_ANALYSIS_PLACEHOLDER =
+  "[Identify and assess potential risks associated with this proposal. Include mitigation strategies for each identified risk.]";
+
+export const SUCCESS_METRICS_PLACEHOLDER =
+  "[Define the metrics that will be used to measure the success of the proposal. How will you track progress and determine if the goals are met?]";
+
+export const COMMUNITY_ENGAGEMENT_PLACEHOLDER =
+  "[Describe how you plan to involve the VebetterDAO community in the proposal. How will you gather feedback and ensure that community members are informed and supportive?]";
+
+export const CONCLUSION_PLACEHOLDER =
+  "[Summarize the key points of the proposal and restate the desired outcome. Encourage community members to participate in the discussion and voting process.]";
+
+export const REFERENCES_PLACEHOLDER = "[List any sources, documents, or links that are referenced in the proposal.]";
+
+export const YOUR_NAME_PLACEHOLDER = "[Your Name]";
+
+export const YOUR_CONTACT_INFO_PLACEHOLDER = "[Your Contact Information]";
+
+export const YOUR_VECHAIN_ADDRESS_PLACEHOLDER = "0xfCf112b20Ac18DeF02ac4c006e6339d2b56Dcf3F";
+
+export const TODAY = dayjs().format("MMM D, YYYY");
