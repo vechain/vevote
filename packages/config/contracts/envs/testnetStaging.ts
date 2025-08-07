5d7ee5999048b820b1d7fa5ef83616688a02cb50
Roisin Dowling <55144940+roisindowling@users.noreply.github.com>
2025-08-07 14:12:55 +0100

    SC: Staging Release (#376)
    update staging addresses

--- packages/config/contracts/envs/testnetStaging.ts
+++ packages/config/contracts/envs/testnetStaging.ts
@@ -3,13 +3,13 @@ export function createTestnetStagingConfig() {
   return defineConfig({
     VITE_APP_ENV: "testnet-staging",
     CONTRACTS_ADMIN_ADDRESS: "0x5a3D02D3e3c47cc646Fac4CC463455eF6aCBbd1a",
-    STARGATE_CONTRACT_ADDRESS: "0x1ec1d168574603ec35b9d229843b7c2b44bcb770", // The contract address of the VeChainNodes contract on mainnet,
-    NODE_MANAGEMENT_CONTRACT_ADDRESS: "0x8bcbfc20ee39c94f4e60afc5d78c402f70b4f3b2", // The contract address of the NodeManagement contract on mainnet
+    STARGATE_CONTRACT_ADDRESS: "0xab65a879185010a59c9c24679e5f1be17ce358c7", // The contract address of the VeChainNodes contract on mainnet,
+    NODE_MANAGEMENT_CONTRACT_ADDRESS: "0x2e970bafffd05d7ececabbdfcfd0866f37278917", // The contract address of the NodeManagement contract on mainnet
     AUTHORITY_CONTRACT_ADDRESS: "0x0000000000000000000000417574686f72697479", // The contract address of the builtin Authority contract on mainnet
     QUORUM_PERCENTAGE: 5, // Quorom set to 5%
-    INITIAL_MIN_VOTING_DELAY: 60480, // 1 week
+    INITIAL_MIN_VOTING_DELAY: 6, // 1 minute
     INITIAL_MAX_VOTING_DURATION: 120960, // 2 weeks
-    INITIAL_MIN_VOTING_DURATION: 60480, // 1 week
-    MIN_VET_STAKE: 1000000000000000000n, // Dawn (1 VET)
+    INITIAL_MIN_VOTING_DURATION: 6, // 1 minute
+    MIN_VET_STAKE: 10000000000000000000000n, // Dawn
   });
 }
