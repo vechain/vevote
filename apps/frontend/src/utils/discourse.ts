c71ef78a8add130099f8d708dd8093dd894d7897
Luca Lo Bianco <94935580+LLPROG@users.noreply.github.com>
2025-08-11 11:38:17 +0200

    Fix discourseUrl for draft proposals (#396)
    fix discourseUrl for draft proposals

--- apps/frontend/src/utils/discourse.ts
+++ apps/frontend/src/utils/discourse.ts
@@ -5,3 +5,7 @@ const discourseBaseUrl = getConfig(import.meta.env.VITE_APP_ENV).discourseBaseUr
 export const getFullDiscourseUrl = (topic: string): string => {
   return `${discourseBaseUrl}${topic}`;
 };
+
+export const getDiscourseTopicUrl = (url: string): string => {
+  return url.replace(discourseBaseUrl, "").replace(/^\//, "");
+};
