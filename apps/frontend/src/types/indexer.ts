900f5f665b7b7b443bfa1a9c417bfa7d89c1985d
roisindowling <roisin.dowling@vechain.org>
2025-09-26 11:41:03 -0400

    Remove legacy indexer url

--- apps/frontend/src/types/indexer.ts
+++ apps/frontend/src/types/indexer.ts
@@ -1,6 +1,5 @@
 export enum IndexerRoutes {
   RESULTS = "/vevote/proposal/results",
   PROPOSAL = "/vevote/proposals/comments",
-  MASTER_NODE = "/authority_endorsers/endorsers",
-  MASTER_NODE2 = "/authority-endorsers/endorsers",
+  MASTER_NODE = "/authority-endorsers/endorsers",
 }
