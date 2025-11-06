816e2613ed78a61ecaa4c12c20f8d87df4283751
“LLPROG” <llobianco@efebia.com>
2025-11-06 08:45:48 +0100

    clean up

--- apps/frontend/src/utils/proposals/historicalProposal.ts
+++ apps/frontend/src/utils/proposals/historicalProposal.ts
@@ -7,10 +7,8 @@ import axios from "axios";
 const indexerUrl = getConfig("mainnet").indexerUrl;
 
 export const getHistoricalProposal = async (proposalId?: string) => {
-  const isValid = !proposalId || (proposalId && proposalId.includes("-"));
-  if (!isValid) {
-    return { results: undefined };
-  }
+  const isValid = !proposalId || proposalId?.includes("-");
+  if (!isValid) return { results: undefined };
   const [contractAddress, id] = proposalId?.split("-") || [undefined, undefined];
   try {
     const res = await axios.get<HistoricalProposalResponse>(`${indexerUrl}${IndexerRoutes.HISTORIC_PROPOSALS}`, {
